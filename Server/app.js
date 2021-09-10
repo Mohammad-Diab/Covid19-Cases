ab4ca4b90fcb2bd9e4ef70f8c68e47afeaf5516a
Mohammad Diab <Mohammad.Diab93@outlook.com>
2021-09-10 17:17:42 +0300

    Use cors and change server port

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
 const host = '127.0.0.1';
-const port = 3000;
+const port = 4000;
+
+app.use(cors())
 
 require('./controller')(app);
-
 app.listen(port, () => {
     console.log(`Example app listening at http://${host}:${port}`)
 })
