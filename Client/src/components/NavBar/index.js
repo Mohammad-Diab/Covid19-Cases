83bdc3f909b2ad391d8d8e1215cbe265188bb131
Mohammad Diab <Mohammad.Diab93@outlook.com>
2021-09-10 14:28:23 +0300

    header component

---
+++ Client/src/components/NavBar/index.js
@@ -0,0 +1,24 @@
+import  Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+import logo from './../../Assets/img/covid-19-logo.png';
+
+function Header(){
+    return (
+        <Navbar bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top"/>
+                    {' '} COVID-19
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Favorite</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header;
