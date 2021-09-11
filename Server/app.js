c495bebdd91fda7268a52afa080b9377b907b097
Mohammad Diab <Mohammad.Diab93@outlook.com>
2021-09-11 22:44:15 +0300

    add logger

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const winston = require('winston');
 
 const app = express();
 const host = '127.0.0.1';
@@ -7,7 +8,17 @@ const port = 4000;
 
 app.use(cors())
 
-require('./controller')(app);
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'log/info.log' }),
+    ],
+});
+
+require('./controller')(app, logger);
 app.listen(port, () => {
     console.log(`Example app listening at http://${host}:${port}`)
 })
--- Server/controller/index.js
+++ Server/controller/index.js
@@ -1,14 +1,22 @@
 const logic = require('../logic');
 const sortBy_enum = require('../enum/sortBy');
 
-module.exports = async function(app) {
+module.exports = async function(app, logger) {
     app.get('/getCountriesList', function(req, res) {
         let filter = req.query.filter || '';
         let pageNumber = req.query.pageNumber || 1;
         let sortBy = req.query.sortBy || sortBy_enum.country;
+        logger.log({
+            level: 'info',
+            message: `${dateTime()}; getCountriesList is requested with these paramenters filter='${filter}' pageNumber='${pageNumber}' sortBy='${sortBy}'`
+        });
         logic.getCountriesList(pageNumber, filter, sortBy).then((result) => {
             res.send(result)
         }).catch((ex) => {
+            logger.log({
+                level: 'error',
+                message: `${dateTime()}; ${ex.message}`
+            });
             res.send({
                 code: 2000,
                 message: ex.message
@@ -20,14 +28,26 @@ module.exports = async function(app) {
 
         let countryId = req.query.countryId || '';
         if (!countryId) {
-            // invalid request;
-            return;
+            logger.log({
+                level: 'warn',
+                message: `${dateTime()}; getCountryDetails; invalid request`
+            });
+            return null;
         }
         let pageNumber = req.query.pageNumber || 1;
+
+        logger.log({
+            level: 'info',
+            message: `${dateTime()}; getCountryDetails is requested with these paramenter pageNumber='${pageNumber}'`
+        });
 
         logic.getCountryDetails(countryId, pageNumber).then((result) => {
             res.send(result)
         }).catch((ex) => {
+            logger.log({
+                level: 'error',
+                message: `${dateTime()}; ${ex.message}`
+            });
             res.send({
                 code: 2000,
                 message: ex.message
@@ -39,13 +59,23 @@ module.exports = async function(app) {
 
         let countryId = req.query.countryId || '';
         if (!countryId) {
-            // invalid request;
-            return;
+            logger.log({
+                level: 'warn',
+                message: `${dateTime()}; getCountryCasesCount; invalid request`
+            });
+            return null;
         }
-
+        logger.log({
+            level: 'info',
+            message: `${dateTime()}; getCountryCasesCount is requested with these paramenter pageNumber='${pageNumber}'`
+        });
         logic.getCountryCasesCount(countryId).then((result) => {
             res.send(result)
         }).catch((ex) => {
+            logger.log({
+                level: 'error',
+                message: `${dateTime()}; ${ex.message}`
+            });
             res.send({
                 code: 2000,
                 message: ex.message
@@ -54,13 +84,27 @@ module.exports = async function(app) {
     });
 
     app.get('/getAllRegions', function(req, res) {
+        logger.log({
+            level: 'info',
+            message: `${dateTime()}; getAllRegions is requested`
+        });
         logic.getAllRegions().then((result) => {
             res.send(result)
         }).catch((ex) => {
+            logger.log({
+                level: 'error',
+                message: `${dateTime()}; ${ex.message}`
+            });
             res.send({
                 code: 2000,
                 message: ex.message
             });
         });
     });
+}
+
+function dateTime() {
+    return new Date().toISOString().
+    replace(/T/, ' ').
+    replace(/\..+/, '')
 }
